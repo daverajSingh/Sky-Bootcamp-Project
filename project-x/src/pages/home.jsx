9c61103f77ff8e870b5edd98f3d488cc2c8eccc9
Ayo <ayo@babalola.com>
2025-09-30 17:52:04 +0100

    CREATE landing page: configured tailwind.css in the config file and added header function to home.jsx

--- project-x/src/pages/home.jsx
+++ project-x/src/pages/home.jsx
@@ -1,13 +1,24 @@
 import { Link } from 'react-router-dom';
 
-const Home = () => {
+const Header = () => {
   return (
     <div>
-      <h1>Home</h1>
-      <Link to="/quiz">Go to Quiz</Link>
-      <Link to="/simulator">Go to Simulator</Link>
-      <Link to="/admin">Go to Admin</Link>
+        <h1 class="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">sky Immersion</h1>
     </div>
   );
 }
+
+const Home = () => {
+  return (
+    <div>
+      {/* <h1>Home</h1>
+      <Link to="/quiz">Go to Quiz</Link>
+      <Link to="/simulator">Go to Simulator</Link>
+      <Link to="/admin">Go to Admin</Link> */}
+      <Header></Header>
+    </div>
+  );
+}
+
+
 export default Home;
