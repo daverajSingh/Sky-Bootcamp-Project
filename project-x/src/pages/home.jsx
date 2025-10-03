a07db17bf6118f4dd17477d34c24c444a76e841f
Ayo <ayo@babalola.com>
2025-10-03 10:07:18 +0100

    CREATED: welcomeSection to include a banner spanning the page, UPDATED: cards.jsx to add the color

--- project-x/src/pages/home.jsx
+++ project-x/src/pages/home.jsx
@@ -1,4 +1,5 @@
 import Header from '../components/header.jsx';
+import WelcomeSection from '../components/welcomeSection.jsx';
 import Card from '../components/card.jsx';
 
 const Home = () => {
@@ -6,6 +7,9 @@ const Home = () => {
     <div>
       <div>
         <Header />
+      </div>
+      <div className="p-6">
+        <WelcomeSection></WelcomeSection>
       </div>
       <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         <Card title="Explore a day at Sky" link="/simulator" description={"Experience our culture, live a day at Sky "}/>
