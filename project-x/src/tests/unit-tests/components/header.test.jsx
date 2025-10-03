acbaa0001544fc20d0da65444fb75a6a611f9df6
Ilia Jamasb <ijamasb03@gmail.com>
2025-10-03 15:17:10 +0100

    CHORE: added describe to header test

--- project-x/src/tests/unit-tests/components/header.test.jsx
+++ project-x/src/tests/unit-tests/components/header.test.jsx
@@ -2,17 +2,18 @@ import Header from "../../../components/header.jsx"
 import { cleanup, fireEvent, render, screen} from '@testing-library/react';
 import React from "react";
 
+describe('Header', () => {
+    afterEach(cleanup);
+    it('Clicking the SkyImmersion text should load the skyImmersion home page', () => {
+        const loadedHomePage = render(
+            <Header />
+        );
 
-afterEach(cleanup);
+        expect(screen.getByText('Sky Immersion')).toBeInTheDocument()
 
-it('Clicking the SkyImmersion text should load the skyImmersion home page', () => {
-    const loadedHomePage = render(
-        <Header />
-    );
+        // fireEvent.click(screen.getByText('Sky Immersion'));
 
-    expect(screen.getByText('Sky Immersion')).toBeInTheDocument()
-
-    // fireEvent.click(screen.getByText('Sky Immersion'));
-
-    // expect(screen.getByRole('link', { name: 'Sky Immersion' })).toHaveAttribute('href', 'https://www.test.com/')
+        // expect(screen.getByRole('link', { name: 'Sky Immersion' })).toHaveAttribute('href', 'https://www.test.com/')
+    });
 });
+
