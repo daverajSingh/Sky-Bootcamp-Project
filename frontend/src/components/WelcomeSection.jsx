f63d72c4746300f707e944750b2aff73291474d0
Sage <sagejacwilliams@yahoo.com>
2025-10-10 13:49:31 +0100

    UPDATE: removed unnecessary divs from Home Page, adjusted welcome section

--- frontend/src/components/WelcomeSection.jsx
+++ frontend/src/components/WelcomeSection.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const WelcomeSection = () => {
 
     return (
-        <div className="relative w-full h-56 rounded-xl overflow-hidden shadow-lg group bg-linear-to-bl from-violet-500 to-fuchsia-500">
+        <div className="relative w-80% h-56 rounded-xl overflow-hidden shadow-lg group bg-linear-to-bl from-violet-500 to-fuchsia-500 m-6">
             <section className="px-6 py-10 text-center">
                 <h1 className="text-8xl">Welcome!</h1>
                 <p className="text-xl">Experience a day at work, take on challenges, and test your skills</p>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,19 +6,13 @@ import FAQPage from '../components/FAQ.jsx';
 const Home = () => {
   return (
     <div>
-      <div>
         <Header />
-      </div>
-      <div className="p-6">
-        <WelcomeSection></WelcomeSection>
-      </div>
+        <WelcomeSection/>
       <div className="p-6 grid grid-cols-2 gap-6">
         <Card title="Explore a day at Sky" link="/simulator" description={"Experience our culture, live a day at Sky "}/>
         <Card title="Are you Sky ready?" link="/quiz" description={"Take the quiz to find out"}/>
       </div>
-      <div>
         <FAQPage />
-      </div>
     </div>
   );
 }
