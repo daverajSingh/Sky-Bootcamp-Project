9ca92d3c8f282c971db5114f5a8068214181fcb1
Daveraj <daveraj.nothay@sky.uk>
2025-10-11 14:49:57 +0100

    REFACTOR: added layout, added footer component, added functionality for login button in header

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Outlet } from "react-router";
+const Layout = () => {
+  return (
+    <div className="flex flex-col w-full min-h-screen">
+      <Header />
+      <main
+        className="pt-30 w-full h-full flex-grow bg-white overflow-scroll md:px-10 lg:px-20"
+        role="main div"
+      >
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
