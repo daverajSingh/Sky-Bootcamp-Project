ec3305324ba7ccd2084c0ef1eb6997f7aefac251
Daveraj <daveraj.nothay@sky.uk>
2025-10-11 14:48:55 +0100

    CHORE: removed redundant nested file

--- frontend/src/__tests__/components/Card.test.jsx
+++ frontend/src/__tests__/components/Card.test.jsx
@@ -1,4 +1,4 @@
-import Card from "../../../components/Card.jsx"
+import Card from "../../components/Card.jsx"
 import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import React from "react";
 
