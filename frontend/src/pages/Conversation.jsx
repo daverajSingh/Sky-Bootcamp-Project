71d6d69aa1a614d2e8217f94484fed6c123b6df8
SaranyaaUK <saranyaajk95@gmail.com>
2025-10-16 22:18:26 +0100

    CHORE: Update the json data to represent topic wise data, Conversation component updated to fetch data accordingly.

--- frontend/src/pages/Conversation.jsx
+++ frontend/src/pages/Conversation.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react"
+import { useParams } from 'react-router-dom';
 import Header from "../components/Header"
 import { ChatContainer, MainContainer, MessageInput, Message, MessageList, MessageSeparator } from "@chatscope/chat-ui-kit-react"
 
 const Conversation = () => {
 
+    const { topicid } = useParams();
     // Order of the messages should be maintained in a list
     const [messageList, setMessageList] = useState([])
     const [userMessageId, setUserMessageId] = useState(1)
@@ -12,31 +14,25 @@ const Conversation = () => {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const today = new Date();
 
-    // let messageList = [{ 'agent': 'AI', 'message': 'Are you ready to discuss about Agile', 'direction': 'incoming' },
-    // { 'agent': 'user', 'message': 'Yes, but I am new to Agile, what is Agile?', 'direction': 'outgoing' },
-    // { 'agent': 'AI', 'message': 'Second message', 'direction': 'incoming' },
-    // { 'agent': 'user', 'message': 'Second message from user john', 'direction': 'outgoing' }
-    // ]
-
     function sendUserResponse(message) {
         // Need to send this to the backend and get a response from the AI
-        fetch('http://localhost:3000/user', {
+        fetch('http://localhost:3000/user-messages', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ 'id': userMessageId, 'message': message }),
+            body: JSON.stringify({ 'id': userMessageId, 'topicID': topicid, 'text': message }),
         })
             .then(() => {
                 // For now, just fetch again the response from the AI - 
                 // when we connect with backend, this should be part of the response from the backend
-                fetch('http://localhost:3000/ai')
+                fetch(`http://localhost:3000/ai-messages?topicID=${topicid}`)
                     .then(response => response.json())
                     .then(data => {
                         console.log(data);
-                        console.log(messageList);
+                        // console.log(messageList);
                         const userResponse = { 'agent': 'user', 'message': message, 'direction': 'outgoing' };
-                        const aiResponse = { 'agent': 'AI', 'message': data[aiMessageId]['message'], 'direction': 'incoming' };
+                        const aiResponse = { 'agent': 'AI', 'message': data[aiMessageId]['text'], 'direction': 'incoming' };
                         setMessageList([...messageList, userResponse, aiResponse]);
                         setAIMessageId(aiMessageId + 1);
                     })
@@ -50,11 +46,10 @@ const Conversation = () => {
 
     useEffect(() => {
         // The first message should be from the AI, asking the user if they are ready to discuss the topic.
-        fetch('http://localhost:3000/ai')
+        fetch(`http://localhost:3000/ai-messages?topicID=${topicid}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data);
-                const message = { 'agent': 'AI', 'message': data[aiMessageId]['message'], 'direction': 'incoming' };
+                const message = { 'agent': 'AI', 'message': data[aiMessageId]['text'], 'direction': 'incoming' };
                 setMessageList([...messageList, message]);
                 setAIMessageId(aiMessageId + 1);
             })
--- frontend/src/pages/Simulator.jsx
+++ frontend/src/pages/Simulator.jsx
@@ -12,7 +12,19 @@ const Simulator = () => {
             <Header />
             <div className="p-6 grid grid-cols-2 gap-6" onClick ={()=> navigate(generatePath("/simulator/1"))}>
                 <Card title="Agile" />
-            </div> 
+            </div>
+            <div className="p-6 grid grid-cols-2 gap-6" onClick ={()=> navigate(generatePath("/simulator/2"))}>
+                <Card title="Compilance" />
+            </div>
+            <div className="p-6 grid grid-cols-2 gap-6" onClick ={()=> navigate(generatePath("/simulator/3"))}>
+                <Card title="Emotional Intelligence" />
+            </div>
+            <div className="p-6 grid grid-cols-2 gap-6" onClick ={()=> navigate(generatePath("/simulator/4"))}>
+                <Card title="Communication" />
+            </div>
+            <div className="p-6 grid grid-cols-2 gap-6" onClick ={()=> navigate(generatePath("/simulator/5"))}>
+                <Card title="Sky Products & Services" />
+            </div>
         </>
     );
 }
