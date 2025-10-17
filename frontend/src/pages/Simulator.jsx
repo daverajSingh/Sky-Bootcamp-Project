c4db02850e608df4d2aab1544945643bc92c9ce0
SaranyaaUK <saranyaajk95@gmail.com>
2025-10-17 11:27:10 +0100

    CHORE: Update Simulator to reuse the onClick added to the

--- frontend/src/pages/Simulator.jsx
+++ frontend/src/pages/Simulator.jsx
@@ -1,30 +1,16 @@
-import { useNavigate, generatePath } from 'react-router-dom';
 import Header from "../components/Header"
 import Card from "../components/Card"
 
 const Simulator = () => {
-
-    const navigate = useNavigate();
-
     return (
         <>
             <Header />
-            <div class="grid grid-flow-col grid-rows-1 gap-4">
-                <div className="p-6" onClick={() => navigate(generatePath("/simulator/1"))}>
-                    <Card title="Agile" />
-                </div>
-                <div className="p-6" onClick={() => navigate(generatePath("/simulator/2"))}>
-                    <Card title="Compilance" />
-                </div>
-                <div className="p-6" onClick={() => navigate(generatePath("/simulator/3"))}>
-                    <Card title="Emotional Intelligence" />
-                </div>
-                <div className="p-6" onClick={() => navigate(generatePath("/simulator/4"))}>
-                    <Card title="Communication" />
-                </div>
-                <div className="p-6" onClick={() => navigate(generatePath("/simulator/5"))}>
-                    <Card title="Sky Products & Services" />
-                </div>
+            <div class="grid grid-flow-col grid-rows-1 gap-4 p-6">
+                <Card title="Agile" link="/simulator/1" />
+                <Card title="Compilance" link="/simulator/2" />
+                <Card title="Emotional Intelligence" link="/simulator/3" />
+                <Card title="Communication" link="/simulator/4" />
+                <Card title="Sky Products & Services" link="/simulator/5" />
             </div>
         </>
     );
