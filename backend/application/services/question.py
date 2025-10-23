9e430d6a95a459c30052fd84a1514446db0e4a31
SaranyaaUK <saranyaajk95@gmail.com>
2025-10-23 21:44:53 +0100

    CHORE: Updated tests for question service and renamed topic service test file

--- backend/application/services/question.py
+++ backend/application/services/question.py
@@ -1,29 +1,42 @@
 from application.data_access import DataAccess
-
+import pymysql
 
 def get_questions():
     db = DataAccess()
-    topics = db.query("SELECT question_id, topic_id, question_text FROM question;")
-
-    return topics
+    try :
+        questions = db.query("SELECT question_id, topic_id, question_text FROM question")
+        return questions
+    except pymysql.MySQLError as e:
+        raise RuntimeError(f'Database query error: {e}')
 
 def get_questions_by_topic_id(topic_id):
     db = DataAccess()
-    topic = db.query("SELECT question_id, question_text FROM question WHERE topic_id = (%s);", topic_id)
 
-    return topic
+    try :
+        question = db.query("SELECT question_id, question_text FROM question WHERE topic_id = (%s)", topic_id)
+        return question
+    except pymysql.MySQLError as e:
+        raise RuntimeError(f'Database query error: {e}')
 
 def add_question(topic_id, question):
     db = DataAccess()
-    db.execute("INSERT INTO question (topic_id, question_text) VALUES (%s, %s)", (topic_id, question))
-
+    try :
+        db.execute("INSERT INTO question (topic_id, question_text) VALUES (%s, %s)", (topic_id, question))
+    except pymysql.MySQLError as e:
+        raise RuntimeError(f'Database query error: {e}')
 
 def delete_question(id):
     db = DataAccess()
-    db.execute("DELETE FROM question WHERE question_id = (%s);", id)
+    try :
+        db.execute("DELETE FROM question WHERE question_id = (%s)", id)
+    except pymysql.MySQLError as e:
+        raise RuntimeError(f'Database query error: {e}')
 
 
 def update_question(id, question):
     db = DataAccess()
-    db.execute("UPDATE question SET question_text = (%s) WHERE question_id = (%s);", (question, id))
+    try :
+        db.execute("UPDATE question SET question_text = (%s) WHERE question_id = (%s)", (question, id))
+    except pymysql.MySQLError as e:
+        raise RuntimeError(f'Database query error: {e}')
     
