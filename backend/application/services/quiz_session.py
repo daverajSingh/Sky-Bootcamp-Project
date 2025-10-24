43fae16f39513f0a4c6ed8c71beb2e7017fa64a6
Ayo <ayo@babalola.com>
2025-10-24 11:45:32 +0100

    CHORE: error catch statements added to quiz_session.py

--- backend/application/services/quiz_session.py
+++ backend/application/services/quiz_session.py
@@ -1,32 +1,53 @@
 from application.data_access import DataAccess
+import pymysql
 
 
 def get_quiz_sessions():
     db = DataAccess()
-    quiz_sessions = db.query("SELECT session_id, start_time, end_time, time_diff FROM quiz_session;")
+    
+    try:
+        quiz_sessions = db.query("SELECT session_id, start_time, end_time, time_diff FROM quiz_session;")
+        return quiz_sessions
+    
+    except pymysql.MySQLError as e:
+        raise RuntimeError(f'Database query error: {e}')
 
-    return quiz_sessions
 
 def get_quiz_session(id):
     db = DataAccess()
-    quiz_session = db.query("SELECT session_id, start_time, end_time, time_diff FROM quiz_session; WHERE session_id = (%s);", id)
 
-    return quiz_session
+    try:
+        quiz_session = db.query("SELECT session_id, start_time, end_time, time_diff FROM quiz_session; WHERE session_id = (%s);", id)
+        return quiz_session
+    
+    except pymysql.MySQLError as e:
+        raise RuntimeError(f'Database query error: {e}')
+
 
 def add_quiz_session(start_time, end_time):
     db = DataAccess()
-    db.execute("INSERT INTO quiz_session (start_time, end_time)" \
-    " VALUES (TIMESTAMP(%s), TIMESTAMP(%s))", (start_time, end_time))
+    try:
+        db.execute("INSERT INTO quiz_session (start_time, end_time)" \
+        " VALUES (TIMESTAMP(%s), TIMESTAMP(%s))", (start_time, end_time))
+
+    except pymysql.MySQLError as e:
+        raise RuntimeError(f'Database query error: {e}')
 
 
 def delete_quiz_sesson(id):
     db = DataAccess()
-    db.query("DELETE FROM quiz_session WHERE session_id = (%s);", id)
+
+    try:
+        db.query("DELETE FROM quiz_session WHERE session_id = (%s);", id)
+    except pymysql.MySQLError as e:
+        raise RuntimeError(f'Database query error: {e}')
 
 
 def update_quiz_session(id, start_time, end_time):
-
     db = DataAccess()
-    quiz_session = db.query("UPDATE quiz_session SET (start_time = (%s), start_time = (%s)) WHERE session_id = (%s);", (start_time, end_time), id)
 
-    return quiz_session
+    try:
+        quiz_session = db.query("UPDATE quiz_session SET (start_time = (%s), start_time = (%s)) WHERE session_id = (%s);", (start_time, end_time), id)
+        return quiz_session
+    except pymysql.MySQLError as e:
+        raise RuntimeError(f'Database query error: {e}')
--- backend/application/services/score.py
+++ backend/application/services/score.py
@@ -1,49 +1,49 @@
 from application.data_access import DataAccess
 import pymysql
 
-
 def get_scores():
     db = DataAccess()
 
     try:
         scores = db.query("SELECT score_id, topic_id, session_id, score_value FROM score;")
-        return scores
-    
+        return scores    
     except pymysql.MySQLError as e:
         raise RuntimeError(f'Database query error: {e}')
 
+
 def get_score(id):
     db = DataAccess()
+
     try:
         score = db.query("SELECT score_id, topic_id, session_id, score_value FROM score; WHERE session_id = (%s);", id)
-        return score
-    
+        return score    
     except:
         raise RuntimeError(f'Database query error: {e}')
 
+
 def add_score(topic_id, session_id, score_value):
     db = DataAccess()
+
     try:
         db.execute("INSERT INTO score (topic_id, session_id, score_value)" \
         " VALUES (%s, %s, %s)", (topic_id, session_id, score_value))
-
     except pymysql.MySQLError as e:
         raise RuntimeError(f'Database query error: {e}')
 
 
 def delete_score(id):
     db = DataAccess()
+
     try:
         db.query("DELETE FROM score WHERE session_id = (%s);", id)
-
     except pymysql.MySQLError as e:
         raise RuntimeError(f'Database query error: {e}')
+
 
 def update_score(id, score_value):
 
     db = DataAccess()
     try:
-        db.query("UPDATE score SET (score_value = (%s)); WHERE score_id = (%s)", score_value, id)
-    
+        db.query("UPDATE score SET (score_value = (%s)); WHERE score_id = (%s)", score_value, id)   
     except pymysql.MySQLError as e:
         raise RuntimeError(f'Database query error: {e}')
