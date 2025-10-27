fddb0e151cdcd0af0dd94ceaf255fa4d514b5103
Ayo <ayo@babalola.com>
2025-10-27 13:09:16 +0000

    CHORE: added time_diff to mock instance query and updated assert statements to reflect quiz_session.py

--- backend/tests/services/test_quiz_session.py
+++ backend/tests/services/test_quiz_session.py
@@ -1,8 +1,8 @@
-from application.services.question import get_quiz_sessions, add_quiz_session, delete_quiz_session, update_quiz_session
+from application.services.quiz_session import get_quiz_sessions, add_quiz_session, delete_quiz_session, update_quiz_session
 import pymysql
 import pytest
 
-DB_ACCESS = 'application.services.question.DataAccess'
+DB_ACCESS = 'application.services.quiz_session.DataAccess'
 
 #create test for get quiz sessions
 def test_get_quiz_sessions(mocker):
@@ -10,21 +10,21 @@ def test_get_quiz_sessions(mocker):
     mock_instance = mock_db.return_value
 
     mock_instance.query.return_value = [
-        {'session_id': 1, 'start_time': '2023-10-01 10:00:00', 'end_time':'2023-10-01 10:30:00'},
-        {'session_id': 2, 'start_time': '2023-10-02 11:00:00', 'end_time':'2023-10-02 11:45:00'}
+        {'session_id': 1, 'start_time': '2025-10-01 10:00:00', 'end_time': '2025-10-01 10:15:00', 'time_diff': '00:15:00'},
+        {'session_id': 2, 'start_time': '2025-10-02 11:00:00', 'end_time': '2025-10-02 11:05:00', 'time_diff': '00:05:00'}
     ]
 
     quiz_sessions = get_quiz_sessions()
     assert len(quiz_sessions) == 2
-    assert quiz_sessions[0]['time_diff'] == '00:30:00'
-    assert quiz_sessions[1]['time_diff'] == '00:45:00'
+    assert quiz_sessions[0]['time_diff'] == '00:15:00'
+    assert quiz_sessions[1]['time_diff'] == '00:05:00'
 
 def test_add_quiz_session(mocker):
     mock_db = mocker.patch(DB_ACCESS)
     mock_instance = mock_db.return_value
     
     add_quiz_session('2023-10-01 10:00:00', '2023-10-01 10:30:00')
-    mock_instance.execute.assert_called_once_with('INSERT INTO quiz_session (start_time, end_time) VALUES (%s),(%s)')
+    mock_instance.execute.assert_called_once_with('INSERT INTO quiz_session (start_time, end_time) VALUES (%s, %s)', ('2023-10-01 10:00:00', '2023-10-01 10:30:00'))
 
 def test_delete_quiz_session(mocker):
     mock_db = mocker.patch(DB_ACCESS)
@@ -38,7 +38,7 @@ def test_update_quiz_session(mocker):
     mock_instance = mock_db.return_value
 
     update_quiz_session(1,'2025-10-27 12:00:00','2025-10-27 12:00:01')
-    mock_instance.execute.assert_called_once_with("UPDATE quiz_session SET start_time = (%s), end_time = (%s) WHERE session_id = (%s)", (1,'2025-10-27 12:00:00','2025-10-27 12:00:01'))
+    mock_instance.execute.assert_called_once_with("UPDATE quiz_session SET start_time = (%s), end_time = (%s) WHERE session_id = (%s)", ('2025-10-27 12:00:00','2025-10-27 12:00:01',1))
 
 def test_update_quiz_session_db_error(mocker):
     mock_db = mocker.patch(DB_ACCESS)
@@ -57,7 +57,7 @@ def test_quiz_session_db_errors(mocker):
     mock_instance.query.side_effect = pymysql.MySQLError("DB Error")
 
     with pytest.raises(RuntimeError) as exc_add:
-        add_quiz_session(1,'2025-10-27 12:00:00','2025-10-27 12:00:01')
+        add_quiz_session('2025-10-27 12:00:00','2025-10-27 12:00:01')
     assert str(exc_add.value) == "Database query error: DB Error"
 
     with pytest.raises(RuntimeError) as exc_delete:
