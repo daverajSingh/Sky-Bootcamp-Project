8c77dc1dde6c9204e9359064f70d0f621f189e27
SaranyaaUK <saranyaajk95@gmail.com>
2025-10-30 15:00:06 +0000

    CREATE: Simulator Route - to call the ai service

---
+++ backend/application/routes/simulator.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, request, jsonify
+# import the ai service 
+
+routes = Blueprint('simulator_routes', __name__, url_prefix='/api')
+
+
+@routes.route('/simulator/<int:topic_id>', methods=['POST'])
+def get_ai_response_for_user_input():
+    data = request.json
+    user_input = data['text']
+    context = data['context']
+
+    #  Call the ai service
+    ai_response = dialogue(user_input, context)
+    return jsonify({"text":ai_response}), 200
+
+
+
+
