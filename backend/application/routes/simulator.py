abc181e208697c57bfdb840a5374d09d3ff76b5e
SaranyaaUK <saranyaajk95@gmail.com>
2025-10-30 18:46:11 +0000

    FIX: Update topics id to match the backend topic-ids in Simulator.jsx

--- backend/application/routes/simulator.py
+++ backend/application/routes/simulator.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, request, jsonify
 from application.services.simulator_ai import dialogue
-# import the ai service 
 
 routes = Blueprint('simulator_routes', __name__, url_prefix='/api')
-
 
 @routes.route('/simulator/<int:topic_id>', methods=['POST'])
 def get_ai_response_for_user_input(topic_id):
@@ -11,7 +9,6 @@ def get_ai_response_for_user_input(topic_id):
     user_input = data['text']
     context = data['context']
 
-    #  Call the ai service
     ai_response = dialogue(user_input, context)
     return jsonify({"text":ai_response}), 200
 
--- frontend/src/pages/Simulator.jsx
+++ frontend/src/pages/Simulator.jsx
@@ -3,9 +3,9 @@ import { Card } from "../components";
 const Simulator = () => {
   return (
     <div className="grid grid-flow-col grid-rows-1 gap-4 p-6">
-      <Card title="Agile" link="/simulator/1" />
-      <Card title="Compilance" link="/simulator/2" />
-      <Card title="Emotional Intelligence" link="/simulator/3" />
+      <Card title="Agile" link="/simulator/2" />
+      <Card title="Compilance" link="/simulator/3" />
+      <Card title="Emotional Intelligence" link="/simulator/1" />
       <Card title="Communication" link="/simulator/4" />
       <Card title="Sky Products & Services" link="/simulator/5" />
     </div>
