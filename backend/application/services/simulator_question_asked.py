e552c154770a3d87911106bad191eb59034cc675
SaranyaaUK <saranyaajk95@gmail.com>
2025-10-31 12:55:02 +0000

    CHORE: Updated tests for the simulator_question_asked service

--- backend/application/services/simulator_question_asked.py
+++ backend/application/services/simulator_question_asked.py
@@ -4,8 +4,8 @@ import pymysql
 def get_simulator_question_asked():
     db = DataAccess()
     try :
-        details = db.query("SELECT question_asked_id, topic_id FROM simulator_question_asked")
-        return details
+        questions_asked = db.query("SELECT question_asked_id, topic_id FROM simulator_question_asked")
+        return questions_asked
     except pymysql.MySQLError as e:
         raise RuntimeError(f'Database query error: {e}')
 
@@ -13,15 +13,15 @@ def get_simulator_question_asked_by_topic_id(topic_id):
     db = DataAccess()
 
     try :
-        detail = db.query("SELECT question_asked_id context FROM simulator_question_asked WHERE topic_id = (%s)", topic_id)
-        return detail
+        questions_asked = db.query("SELECT question_asked_id context FROM simulator_question_asked WHERE topic_id = (%s)", topic_id)
+        return questions_asked
     except pymysql.MySQLError as e:
         raise RuntimeError(f'Database query error: {e}')
 
 def add_simulator_question_asked(topic_id):
     db = DataAccess()
     try :
-        db.execute("INSERT INTO simulator_question_asked (topic_id) VALUES (%s)", (topic_id))
+        db.execute("INSERT INTO simulator_question_asked (topic_id) VALUES (%s)", topic_id)
     except pymysql.MySQLError as e:
         raise RuntimeError(f'Database query error: {e}')
 
@@ -35,7 +35,7 @@ def delete_simulator_question_asked(question_asked_id):
 def update_simulator_question_asked(question_asked_id, topic_id):
     db = DataAccess()
     try :
-        db.execute("UPDATE simulator_question_asked SET topic_id = (%s) WHERE question_asked_id = (%s)", (question_asked_id))
+        db.execute("UPDATE simulator_question_asked SET topic_id = (%s) WHERE question_asked_id = (%s)", (topic_id, question_asked_id))
     except pymysql.MySQLError as e:
         raise RuntimeError(f'Database query error: {e}')
     
--- backend/tests/services/test_simulator_question_asked.py
+++ backend/tests/services/test_simulator_question_asked.py
@@ -1,4 +1,4 @@
-from application.services.simulator_question_asked import get_simulator_question_asked, add_simulator_question_asked, delete_simulator_question_asked, update_simulator_question_asked
+from application.services.simulator_question_asked import get_simulator_question_asked, add_simulator_question_asked, delete_simulator_question_asked, update_simulator_question_asked, get_simulator_question_asked_by_topic_id
 import pymysql
 import pytest
 
@@ -22,3 +22,68 @@ def test_get_simulator_question_asked(mocker):
     assert questions_asked[0]['question_asked_id'] == 1
     assert questions_asked[2]['topic_id'] == 2
     assert questions_asked[2]['question_asked_id'] == 3
+
+def test_get_simulator_question_asked_by_topic_id(mocker):
+    mock_db = mocker.patch(DB_ACCESS)
+    mock_instance = mock_db.return_value
+
+    mock_instance.query.return_value = [
+        {'question_asked_id': 3, 'topic_id': 2},
+        {'question_asked_id': 4, 'topic_id': 2}
+    ]
+
+    questions_asked = get_simulator_question_asked_by_topic_id(1)
+
+    assert len(questions_asked) == 2
+    assert questions_asked[0]['topic_id'] == 2
+    assert questions_asked[0]['question_asked_id'] == 3
+    assert questions_asked[1]['topic_id'] == 2
+    assert questions_asked[1]['question_asked_id'] == 4
+
+
+def test_add_simulator_question_asked(mocker):
+    mock_db = mocker.patch(DB_ACCESS)
+    mock_instance = mock_db.return_value
+
+    add_simulator_question_asked(1)
+    mock_instance.execute.assert_called_once_with("INSERT INTO simulator_question_asked (topic_id) VALUES (%s)", 1)
+
+def test_delete_simulator_question_asked(mocker):
+    mock_db = mocker.patch(DB_ACCESS)
+    mock_instance = mock_db.return_value
+
+    delete_simulator_question_asked(1)
+    mock_instance.execute.assert_called_once_with("DELETE FROM simulator_question_asked WHERE question_asked_id = (%s)", 1)
+
+def test_update_simulator_question_asked(mocker):
+    mock_db = mocker.patch(DB_ACCESS)
+    mock_instance = mock_db.return_value
+
+    update_simulator_question_asked(1, 1)
+    mock_instance.execute.assert_called_once_with("UPDATE simulator_question_asked SET topic_id = (%s) WHERE question_asked_id = (%s)", (1,1))
+
+def test_simulator_question_asked_db_errors(mocker):
+    mock_db = mocker.patch(DB_ACCESS)
+    mock_instance = mock_db.return_value
+    mock_instance.execute.side_effect = pymysql.MySQLError("DB Error")
+    mock_instance.query.side_effect = pymysql.MySQLError("DB Error")
+
+    with pytest.raises(RuntimeError) as exc_add:
+        add_simulator_question_asked(1)
+    assert str(exc_add.value) == "Database query error: DB Error"
+
+    with pytest.raises(RuntimeError) as exc_delete:
+        delete_simulator_question_asked(1)
+    assert str(exc_delete.value) == "Database query error: DB Error"
+
+    with pytest.raises(RuntimeError) as exc_update:
+        update_simulator_question_asked(1, 1)
+    assert str(exc_update.value) == "Database query error: DB Error"
+
+    with pytest.raises(RuntimeError) as query_get:
+        get_simulator_question_asked()
+    assert str(query_get.value) == "Database query error: DB Error"
+
+    with pytest.raises(RuntimeError) as query_get_by_id:
+        get_simulator_question_asked_by_topic_id(1)
+    assert str(query_get_by_id.value) == "Database query error: DB Error"
