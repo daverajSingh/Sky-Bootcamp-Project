b91ff061afcfa3aa5054a42834ce734c6bf08343
SaranyaaUK <saranyaajk95@gmail.com>
2025-11-04 19:39:10 +0000

    CHORE: Add events and rendering function

--- frontend/src/pages/Simulator.jsx
+++ frontend/src/pages/Simulator.jsx
@@ -3,6 +3,57 @@ import FullCalendar from '@fullcalendar/react'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 
+// YYYY-MM-DD of today
+let todayStr = new Date().toISOString().replace(/T.*$/, '') 
+
+const EVENTS = [
+  {
+    title: 'Emotional Intelligence Workshop',
+    start: todayStr + 'T10:45:00',
+    end: todayStr + 'T11:45:00',
+    extendedProps: {
+      description: 'Led by the Empathetic Coach. Learn how emotional intelligence improves workplace relationships and performance.',
+      link: "/simulator/1"
+    },
+  },
+  {
+    title: 'Agile Methodology Deep Dive',
+    start: todayStr + 'T13:30:00',
+    end: todayStr + 'T14:30:00',
+    extendedProps: {
+      description: 'Hosted by the Agile Scrum Master. Explore sprints, adaptive planning, and continuous improvement.',
+      link: "/simulator/2",
+    },
+  },
+  {
+    title: 'Compliance & Ethics Briefing',
+    start: todayStr + 'T09:30:00',
+    end: todayStr + 'T10:30:00',
+    extendedProps: {
+      description: 'Presented by the Compliance Officer. Understand regulatory compliance and ethical standards in business.',
+      link: "/simulator/3",
+    },
+  },
+  {
+    title: 'Communication Skills for Leaders',
+    start: todayStr + 'T15:00:00',
+    end: todayStr + 'T16:00:00',
+    extendedProps: {
+      description: 'Facilitated by the Communication Specialist. Learn strategies for collaboration, conflict resolution, and leadership.',
+      link: "/simulator/4"
+    },
+  },
+  {
+    title: 'Sky Product Showcase',
+    start: todayStr + 'T16:15:00',
+    end: todayStr + 'T17:15:00',
+    extendedProps: {
+      description: 'Presented by the Sky Product Expert. Discover Sky’s latest offerings and how they enhance customer experience.',
+      link: "/simulator/5"
+    },
+  }
+]
+
 
 const Simulator = () => {
   return (
@@ -31,11 +82,31 @@ const Simulator = () => {
           slotMinTime={"08:00:00"}
           slotMaxTime={"20:00:00"}
           height={"auto"}
+          initialEvents={EVENTS}
+          eventContent={renderEventContent}
         />
       </div>
     </>
-   
+
   );
 };
 
+
+function renderEventContent(eventInfo) {
+  const { timeText, event } = eventInfo;
+  const { description } = event.extendedProps;
+  return (
+    <>
+      <b>{timeText} </b>
+      <i>{event.title}</i>
+      {description && (
+        <div className="py-1 text-xs text-gray-600">
+          {description}
+        </div>
+      )}
+    </>
+  )
+}
+
 export default Simulator;
+
